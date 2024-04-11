c5e06753b99cf174044ed370105af1655fc925bf
CyberKilljoy <darkmilk85@gmail.com>
2024-04-11 22:19:36 +0300

    first commit

---
+++ initial/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+  <my-components />
+</div>
+</template>
+
+<script>
+import MyComponents from './components/MyComponents'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponents
+
+  }
+}
+</script>
+
